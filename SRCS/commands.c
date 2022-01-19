b8778b1fa15368479bbf00ddebbd98e0d4cbf596
Leo BESNARD <lbesnard@e2r3p18.clusters.42paris.fr>
2022-01-19 21:13:58 +0100

    push swap on avance

--- SRCS/commands.c
+++ SRCS/commands.c
@@ -6,14 +6,65 @@
 /*   By: lbesnard <lbesnard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 14:39:46 by lbesnard          #+#    #+#             */
-/*   Updated: 2022/01/18 22:27:10 by lbesnard         ###   ########.fr       */
+/*   Updated: 2022/01/19 20:58:11 by lbesnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	sa(t_stack **stack_a)
+int	sab(t_stack **head)
 {
-	t_stack	*tmp;
+	t_stack	*stack;
 	
+	stack = *head;
+	if (stack && stack->next)
+		ft_swap(&stack->num, &stack->next->num);
+	return (0);
+}
+
+int	pab(t_stack **head_to,t_stack **head_from)
+{
+	t_stack	*to;
+	t_stack	*from;
+	
+	if (from)
+	{
+		to = *head_to;
+		from = *head_from;
+		*head_from = from->next;
+		from->next = to;
+		*head_to = from;
+	}
+	return (0);
+}
+
+int	rab(t_stack **head)
+{
+	t_stack	*stack;
+
+	stack = *head;
+	if (!*head)
+		return (0);
+	while (stack->next)
+	{
+		ft_swap(stack->num,stack->next->num);
+		stack = stack->next;
+	}
+	return (0);
+}
+
+int	rrab(t_stack **head)
+{
+	t_stack	*last;
+	t_stack	*stack;
+	
+	stack = *head;
+	while(stack->next->next)
+	{
+		stack = stack->next;
+	}
+	last = stack->next;
+	stack->next = NULL;
+	last->next = *head;
+	*head = last;
 }
