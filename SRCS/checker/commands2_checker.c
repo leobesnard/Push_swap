9a7162f9c106d4cb93081099c325d8e493f4fd0b
Leo BESNARD <lbesnard@e2r7p7.clusters.42paris.fr>
2022-03-17 13:55:05 +0100

    FINI

--- SRCS/checker/commands2_checker.c
+++ SRCS/checker/commands2_checker.c
@@ -6,7 +6,7 @@
 /*   By: lbesnard <lbesnard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 16:43:01 by lbesnard          #+#    #+#             */
-/*   Updated: 2022/03/15 17:27:36 by lbesnard         ###   ########.fr       */
+/*   Updated: 2022/03/17 12:35:47 by lbesnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 int	ft_ss(t_list **stack_a, t_list **stack_b)
 {
+	if (!*stack_a || !*stack_b)
+		return (0);
 	ft_sab(stack_a);
 	ft_sab(stack_b);
 	return (0);
@@ -22,6 +24,8 @@ int	ft_ss(t_list **stack_a, t_list **stack_b)
 
 int	ft_rr(t_list **stack_a, t_list **stack_b)
 {
+	if (!*stack_a || !*stack_b)
+		return (0);
 	ft_rab(stack_a);
 	ft_rab(stack_b);
 	return (0);
@@ -29,6 +33,8 @@ int	ft_rr(t_list **stack_a, t_list **stack_b)
 
 int	ft_rrr(t_list **stack_a, t_list **stack_b)
 {
+	if (!*stack_a || !*stack_b)
+		return (0);
 	ft_rrab(stack_a);
 	ft_rrab(stack_b);
 	return (0);
